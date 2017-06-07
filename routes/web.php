c5362b16d6e3a835a46f67fac7a80ceedeaed348
kalpitakhawat <kalpitakhawat@gmail.com>
2017-06-07 10:38:45 +0530

    api route group added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'api','namespace'=>'api'], function () {
+    Route::post('search','SearchController@index');
+});
