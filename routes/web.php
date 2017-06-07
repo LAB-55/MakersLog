4a3b3a142212e00708858030e9246346ac4e8d92
kalpitakhawat <kalpitakhawat@gmail.com>
2017-06-07 10:45:46 +0530

    api route group added

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+$meta = [
+		'title' => "Search Makers Log ",
+		'pageName' => 'Search',
+		'firstName' => 'Kalpit',
+		'lastName' => 'Akhawat',
+		'gusermail' => 'kalpitakhawat',
+		'avatar'	=> 'https://avatars0.githubusercontent.com/u/16951479?v=3&s=460'
+	];
+	return view('search')
+    			->with('meta',$meta);;
+});
+Route::group(['prefix' => 'api','namespace'=>'api'], function () {
+    Route::post('search','SearchController@index');
+});
+
+Route::get('/{gusermail}', function ( $gusermail) {
+
+	$meta = [
+		'title' => "Kalpit Akhawat's Log",
+		'pageName' => 'Kalpit Akhawat',
+		'firstName' => 'Kalpit',
+		'lastName' => 'Akhawat',
+		'gusermail' => 'kalpitakhawat',
+		'avatar'	=> 'https://avatars0.githubusercontent.com/u/16951479?v=3&s=460'
+	];
+    return view('welcome')
+    			->with('meta',$meta);
+});
+
+Route::get('/log/new', function () {
+
+	$meta = [
+		'title' => "New log - Kalpit Akhawat",
+		'pageName' => 'Kalpit Akhawat / New Log ',
+		'firstName' => 'Kalpit',
+		'lastName' => 'Akhawat',
+		'gusermail' => 'kalpitakhawat',
+		'avatar'	=> 'https://avatars0.githubusercontent.com/u/16951479?v=3&s=460'
+	];
+    return view('newpost')
+    			->with('meta',$meta);
+});
