4e1ee65bf41ae5750e91e6c3fccc9330a29e412c
kalpitakhawat <kalpitakhawat@gmail.com>
2017-06-07 18:16:56 +0530

    category retrival done

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use app\Category;
+use App\Category;
 
 class CategoryController extends Controller
 {
