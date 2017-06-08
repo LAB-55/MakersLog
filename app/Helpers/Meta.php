cd524853d79f78a2f7bd6dbde145aaa3d412c30b
kalpitakhawat <kalpitakhawat@gmail.com>
2017-06-08 13:36:38 +0530

    meta auth check fixed

--- app/Helpers/Meta.php
+++ app/Helpers/Meta.php
@@ -8,13 +8,21 @@ class Meta
 {
     public static function get( $title ){
         // Request::
-       return [
+        if (Auth::check()) {
+                return [
+                'title' => $title,
+                'pageName' => $title,
+                'firstName' => Auth::user()->first_name,
+                'lastName' => Auth::user()->last_name,
+                'gusermail' => Auth::user()->g_username,
+                'avatar'    => str_replace("sz=100", "" , Auth::user()->avatar) 
+            ];
+        }
+        return [
             'title' => $title,
-            'pageName' => $title,
-            'firstName' => Auth::user()->first_name,
-            'lastName' => Auth::user()->last_name,
-            'gusermail' => Auth::user()->g_username,
-            'avatar'    => str_replace("sz=100", "" , Auth::user()->avatar) 
+            'pageName' => $title, 
         ];
+        
+       
     }
 }
