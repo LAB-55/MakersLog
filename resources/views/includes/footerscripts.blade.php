459a4b5e3d9240593de0362bfdef852cc1d61637
Jimish Fotariya <fotariyajimish@gmail.com>
2017-06-09 20:29:36 +0530

    category scroll top implementations

--- resources/views/includes/footerscripts.blade.php
+++ resources/views/includes/footerscripts.blade.php
@@ -5,6 +5,7 @@
 	}
 </script>
 <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="/js/clamp.min.js"></script>
 <script type="text/javascript" src="/js/tether.min.js"></script>
 <script type="text/javascript" src="/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="/js/mdb.min.js"></script>
