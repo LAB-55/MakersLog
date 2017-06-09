b04383dd371f22cb287a14aabd50c89106c55013
Devang Bhuva <devangbhuva123@yahoo.com>
2017-06-09 17:40:30 +0530

    logs api

---
+++ app/Http/Controllers/api/LogsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use App\Post;
+
+class LogsController extends Controller
+{
+    public function index($gusermail)
+    {
+    	try {
+
+	    	$user = User::Select('*')->Where('g_username', $gusermail)->first();
+	    	if (isset($user->provider_id)) {
+	    		$logs = Post::Select('*')
+	    						->Where('provider_id', $user->provider_id)
+	    						->Where('is_latest', '1')
+	    						->Where('delete', '0')->get();
+		    	
+		    	return ['status'=>'1' , "collection" =>$logs ];
+	    	}
+		    return ['status'=>'0' , "error" =>'404' ];	
+    	}
+    	catch (Exception $e) {
+      		return json(['status'=>'0','error'=>$e]);
+      	}
+
+    }
+}
