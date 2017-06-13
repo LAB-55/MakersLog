424b12d6ff468cdf0fff6c2566d54951328c7275
Jimish Fotariya <fotariyajimish@gmail.com>
2017-06-13 21:17:29 +0530

    categories in individual blog

--- resources/views/individual.blade.php
+++ resources/views/individual.blade.php
@@ -22,7 +22,7 @@
                     <div class="row">
                         <div class="col-md-2 col-sm-2 text-right">
                             
-                            <a target="_blank" href="/{{ $u->g_username}}" class="media-left waves-light waves-effect waves-light"><img src="https://lh4.googleusercontent.com/-yIfvqErBAzA/AAAAAAAAAAI/AAAAAAAAAjg/DJ2vTnUnDdE/photo.jpg?sz=100" alt="image of {{$p->first_name}}" width="80" class="rounded-circle-imp"></a>
+                            <a target="_blank" href="/{{ $u->g_username }}" class="media-left waves-light waves-effect waves-light"><img src="https://lh4.googleusercontent.com/-yIfvqErBAzA/AAAAAAAAAAI/AAAAAAAAAjg/DJ2vTnUnDdE/photo.jpg?sz=100" alt="image of {{$p->first_name}}" width="80" class="rounded-circle-imp"></a>
                         </div>
                         <div class="col-md-10 col-sm-10">
                             <h3 class="section-heading text-left"> {{ $p->p_title}}</h3>
@@ -49,8 +49,55 @@
     </main>
     <!--/Main layout-->
 
-    <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
-        <a data-toggle="tooltip" data-placement="left" title="Add new log" class="btn-floating btn-large red">
+
+<footer class="page-footer blue center-on-small-only">
+
+    <!--Footer Links-->
+    <div class="container-fluid">
+        <div class="row">
+
+            <!--First column-->
+            <div class="col-md-8">
+                <h5 class="title">Tags</h5>
+                <p><?php 
+                    if( strlen($p->categories) > 1 ){
+                        $ar = explode(",",$p->categories);
+                        $l = count( $ar );
+                        for($i = 0; $i < $l - 1 ; $i++) {
+                            $val = $ar[$i];
+                            echo "<a target='_blank' class='btn btn-outline-secondary light-blue lighten-4 waves-effect ' href='/?cat=".$val."'>".$val."</a>";
+                            
+                        }
+                    }
+                ?></p>
+            </div>
+            <!--/.First column-->
+
+            <!--Second column-->
+            <div class="col-md-4">
+                <h5 class="title">Links</h5>
+                <ul>
+                    <li><a href="/{{$u->g_username}}">More from {{ $u->first_name }} </a></li>
+                    <li><a href="/">MakersLog Home </a></li>
+                </ul>
+            </div>
+            <!--/.Second column-->
+        </div>
+    </div>
+    <!--/.Footer Links-->
+
+    <!--Copyright-->
+    <div class="footer-copyright">
+        <div class="container-fluid">
+            © 2017 Team MakersLog
+        </div>
+    </div>
+    <!--/.Copyright-->
+
+</footer>
+
+    <div class="fx-action-btn" style="bottom: 45px; right: 24px;">
+        <a href="/log/new" data-toggle="tooltip" data-placement="left" title="Add new log" class="btn-floating btn-large red">
             <i class="fa fa-pencil"></i>
         </a>
     </div>
