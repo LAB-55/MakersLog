cbde60aa556b82eeb0581ebb257914dd935dc7d9
kavanpancholi <kavanpancholi@gmail.com>
2017-06-14 17:17:24 +0530

    fix carbon date for human dates

--- app/Http/Controllers/api/LogsController.php
+++ app/Http/Controllers/api/LogsController.php
@@ -11,24 +11,27 @@ class LogsController extends Controller
 {
     public function index($gusermail)
     {
-    	try {
-
-	    	$user = User::Select('*')->Where('g_username', $gusermail)->first();
-	    	if (isset($user->provider_id)) {
-	    		$logs = Post::Select('*')
-	    						->Where('provider_id', $user->provider_id)
-	    						->Where('is_latest', '1')
-	    						->Where('delete', '0')
-								->orderBy('created_at', 'desc')
-	    						->get();
-		    	
-		    	return ['status'=>'1' , "collection" =>$logs ];
-	    	}
-		    return ['status'=>'0' , "error" =>'404' ];	
-    	}
-    	catch (Exception $e) {
-      		return json(['status'=>'0','error'=>$e]);
-      	}
+        try {
+            $user = User::where('g_username', $gusermail)->first();
+            if (isset($user->provider_id)) {
+                $logs = Post::where('provider_id', $user->provider_id)
+                    ->where('is_latest', '1')
+                    ->where('delete', '0')
+                    ->orderBy('created_at', 'desc')
+                    ->get();
+                if ($logs) {
+                    $logs = collect($logs)->map(function ($item) {
+                        $data               = $item->toArray();
+                        $data['created_at'] = $item->created_at->diffForHumans();
+                        return $data;
+                    });
+                }
+                return ['status' => '1', "collection" => $logs];
+            }
+            return ['status' => '0', "error" => '404'];
+        } catch (Exception $e) {
+            return json(['status' => '0', 'error' => $e]);
+        }
 
     }
 }
