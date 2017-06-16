2462b07d035b2a9a436d0e6d3513d6171f92a4c9
Devang Bhuva <devangbhuva123@yahoo.com>
2017-06-16 11:40:51 +0530

    profile image round done

--- app/Helpers/Meta.php
+++ app/Helpers/Meta.php
@@ -17,7 +17,7 @@ class Meta
                 'firstName' => Auth::user()->first_name,
                 'lastName' => Auth::user()->last_name,
                 'gusermail' => Auth::user()->g_username,
-                'avatar'    => str_replace("sz=100", "" , Auth::user()->avatar),
+                'avatar'    => Auth::user()->avatar,
                 'website' => Auth::user()->website,
                 'bio' => Auth::user()->bio,
                 'gender' => Auth::user()->gender,
