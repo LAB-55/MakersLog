075b34d25076ab6cc6a0424ecaafedd2a2f7d623
devangbhuva97 <devangbhuva123@yahoo.com>
2017-06-25 02:18:27 +0530

    GitHub Contributor Mobile View Updated - Class name mistake

--- resources/views/contributor.blade.php
+++ resources/views/contributor.blade.php
@@ -15,7 +15,7 @@
         <div class="container-fluid">
             <div class="row">
             @for ($i = (count($contributors))-1; $i >= 0; $i--)
-                <div class="contributor-card col-sm-6 githubProfileCard row">
+                <div class="contributor-card col-sm-6 githubCard row">
                     <div class="col-sm-5 githubProfile">
                         <div class="githubProfileCard card testimonial-card">
                             <div class="github avatar">
